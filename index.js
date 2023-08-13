3803829d1827dad181b15450d64f4b28b67ca2be
joelnyongesa <joelnyongesa148@gmail.com>
2023-08-13 12:34:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(string){
+    const lowercaseWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+        
+        const words = string.split(' ').map((word, index) => {
+            if (index === 0 || !lowercaseWords.includes(word.toLowerCase())) {
+                return word.charAt(0).toUpperCase() + word.slice(1);
+            } else {
+                return word.toLowerCase();
+            }
+        });
+        
+        return words.join(' ');
+  }
 }
